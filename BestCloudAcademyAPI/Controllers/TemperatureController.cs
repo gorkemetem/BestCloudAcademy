831f3d3399e698e4596f5297e5669104f43cada1
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2022-02-04 16:37:48 +0300

    Add project files.

---
+++ BestCloudAcademyAPI/Controllers/TemperatureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestCloudAcademyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
