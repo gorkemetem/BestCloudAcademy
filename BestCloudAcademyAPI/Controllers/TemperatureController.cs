9c4b5a43574698aae360b3916939873ab28e9d76
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2022-02-05 21:37:52 +0300

    Test2

--- BestCloudAcademyAPI/Controllers/TemperatureController.cs
+++ BestCloudAcademyAPI/Controllers/TemperatureController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BestCloudAcademyAPI.Controllers
 {
@@ -21,9 +22,13 @@ namespace BestCloudAcademyAPI.Controllers
         }
 
         [HttpGet("temperature")]
-        public string Get(string city)
+        public string Get()
         {
-            return "DENEME";
+            string api = "088311902dbed3f3e2094d195a47cec6";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument weather = XDocument.Load(connection);
+            var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            return "temperature = " + temp;
         }
     }
 }
