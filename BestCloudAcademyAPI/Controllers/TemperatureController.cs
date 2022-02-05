14eb9c3cac9b2260c24e7e94b200de4cb220ebbd
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2022-02-05 21:58:00 +0300

    Test5

--- BestCloudAcademyAPI/Controllers/TemperatureController.cs
+++ BestCloudAcademyAPI/Controllers/TemperatureController.cs
@@ -13,22 +13,23 @@ namespace BestCloudAcademyAPI.Controllers
     public class TemperatureController : ControllerBase
     {
 
-        Person person1 = new Person { FirstName = "Görkem Etem", LastName = "Irmak" };
-
         [HttpGet("")]
         public Person Get()
         {
+            Person person1 = new Person { FirstName = "Görkem Etem", LastName = "Irmak" };
             return person1;
         }
 
         [HttpGet("temperature")]
-        public string Get(string city)
+        public Weather Get(string city)
         {
             string api = "088311902dbed3f3e2094d195a47cec6";
             string connection = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&mode=xml&lang=tr&units=metric&appid=" + api;
             XDocument weather = XDocument.Load(connection);
             var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
-            return "temperature for " + city + " = " + temp;
+            Weather weather1 = new Weather();
+            weather1.Temperature = temp;
+            return weather1;
         }
     }
 }
